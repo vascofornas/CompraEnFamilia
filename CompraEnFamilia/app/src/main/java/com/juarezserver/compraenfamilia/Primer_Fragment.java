caac599d26e853650a3ca2ddbbd274a7a2b7826c
Modesto-PC\Modesto <desarrollo@webjuarez.com>
2016-11-17 01:35:27 -0700

    SINCRONIZAR

---
+++ CompraEnFamilia/app/src/main/java/com/juarezserver/compraenfamilia/Primer_Fragment.java
@@ -0,0 +1,23 @@
+package com.juarezserver.compraenfamilia;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Modesto on 11/9/2016.
+ */
+
+public class Primer_Fragment extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.primer_layout,container,false);
+        return myView;
+    }
+}
